7cb2607c8adc831d884d699cce560a1aabfedd99
GarethBeer <gareth.beer1989@outlook.com>
2020-03-02 18:47:32 +0000

    adding axios requests - send,post and delete

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,58 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
+	state = {
+		posts: [],
+		selectedPost: null
+	};
+	componentDidMount() {
+		axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+			const posts = response.data.slice(0, 4);
+			const updatedPosts = posts.map(post => {
+				return {
+					...post,
+					author: "Gareth"
+				};
+			});
+			this.setState({
+				posts: updatedPosts
+			});
+		});
+	}
+
+	deletePostHandler = () => {
+		axios
+			.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+			.then(response => console.log(response));
+	};
+
+	clickPostHandler = id => {
+		this.setState({
+			selectedPost: id
+		});
+	};
 	render() {
+		const posts = this.state.posts.map(post => {
+			return (
+				<Post
+					key={post.id}
+					title={post.title}
+					author={post.author}
+					clicked={() => this.clickPostHandler(post.id)}
+				/>
+			);
+		});
 		return (
 			<div>
-				<section className="Posts">
-					<Post />
-					<Post />
-					<Post />
-				</section>
+				<section className="Posts">{posts}</section>
 				<section>
-					<FullPost />
+					<FullPost
+						id={this.state.selectedPost}
+						delete={this.deletePostHandler}
+					/>
 				</section>
 				<section>
 					<NewPost />
